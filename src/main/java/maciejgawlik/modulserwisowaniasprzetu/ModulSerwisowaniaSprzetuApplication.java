21aaedc0a83b5a4d4f06d96d919e43e898d9b3cb
m <g>
2019-11-01 08:16:56 +0100

    initial

---
+++ src/main/java/maciejgawlik/modulserwisowaniasprzetu/ModulSerwisowaniaSprzetuApplication.java
@@ -0,0 +1,13 @@
+package maciejgawlik.modulserwisowaniasprzetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulSerwisowaniaSprzetuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulSerwisowaniaSprzetuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maciejgawlik/modulserwisowaniasprzetu/ModulSerwisowaniaSprzetuApplicationTests.java
@@ -0,0 +1,13 @@
+package maciejgawlik.modulserwisowaniasprzetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulSerwisowaniaSprzetuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
